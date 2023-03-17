06ec3bffbeff2b021cca0fef45654ccdf45c6050
edmond-k <edmondthedev@gmail.com>
2023-03-17 17:45:20 +0300

    Front-end
    Front-end code

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const { testnet, mainnet } = require("bitcore-lib/lib/networks");
-const { createWallet, createHDWallet } = require("./wallet.bitcoin");
-const sendPayment = require("./send.bitcoin");
-
-
-
-sendPayment("bananas", 1)
-
-// console.log(createHDWallet(testnet))
